e07ed05c9ec660595ded7d1e24e5a4e9846721a1
fabianofrank <fabianofrank@protonmail.com>
2021-11-09 08:32:44 -0300

    Add ESlint inline comment and descriptive function name

--- menu.js
+++ menu.js
@@ -1,4 +1,5 @@
-function myFunction() {
+// eslint-disable-next-line no-unused-vars
+function mobileMenu() {
   const div = document.getElementById('menu');
   if (div.style.display === 'block') {
     div.style.display = 'none';
